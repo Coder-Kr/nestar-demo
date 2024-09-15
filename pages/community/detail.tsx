5b89527b9ed70147b3fe67ab788da7fc2e24ff89
Coder-Kr <jasurbekodilovn@mail.ru>
2024-09-15 11:19:44 +0900

    feat: organize and test router system

---
+++ pages/community/detail.tsx
@@ -0,0 +1,5 @@
+const Detail = () => {
+   return <div>Community Detail</div>;
+};
+
+export default Detail;
---
+++ pages/property/[productId]/index.tsx
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router";
+
+const PropertyDetail = () => {
+   const router = useRouter();
+   const { productId } = router.query;
+
+   return <div>PROPERTY DETAILl {productId}</div>;
+};
+
+export default PropertyDetail;
